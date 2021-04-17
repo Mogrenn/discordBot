49c844f6b233af2b1cb96fb493ba267ba3943966
Brandon Mogren <mogren@hotmail.se>
2021-04-17 21:10:38 +0200

    I have added higher or lower gambling game

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const botChannel = "832950711691247636";
 let queue = [];
 let volume = 0.1;
 let isPlaying = false;
+let gamblers = [];
 
 const Text = {
     userNotInChannel: "Your not in a voice channel"
@@ -26,14 +27,21 @@ client.on("message", (msg) => {
 
 function commandResolver(command, msg) {
 
+    //TODO: Refactor this to be more useful
     if (command.match(new RegExp("https"))) {
         command = "!play";
     }
+
+    if (command.match(new RegExp(" "))) {
+        command = command.split(" ")[0];
+
+    }
+
     switch (command.split("!")[1]) {
         case 'test':
             sendMessageToBotChannel(msg);
             break;
-        case 'play':
+        case 'play', 'p':
             addSongToQueue(msg);
             break;
         case 'leave':
@@ -50,6 +58,12 @@ function commandResolver(command, msg) {
             break;
         case 'skipnext':
             skipNextSong();
+            break;
+        case 'higherorlower':
+            higherOrLower(msg);
+            break;
+        case "guess":
+            guess(msg);
             break;
         default:
             msg.channel.send("You are an idiot");
@@ -131,12 +145,23 @@ async function playMusic(connection) {
     })
 }
 
-function checkIfUserInChannel(msg) {
-    if (!msg.member.voice.channel)
-        return false;
-    else {
-        return true
+function higherOrLower(msg) {
+    gamblers.push(new HigherOrLower(msg));
+}
+
+function guess(msg) {
+
+    for (const player of gamblers) {
+        if (msg.author === player.getAuthor()) {
+            let guess = msg.content.split(" ")[1];
+            player.playerGuess(guess);
+            gamblers.splice(player);
+        }
     }
+}
+
+function checkIfUserInChannel(msg) {
+    return msg.member.voice.channel;
 }
 
 function changeVolume(msg) {
@@ -144,6 +169,42 @@ function changeVolume(msg) {
 
     let newVolume = parseFloat(msg.content.split(" ")[0]) * 0.001;
     volume = newVolume;
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+class HigherOrLower {
+    constructor(msg) {
+        this.msg = msg;
+        this.author = msg.author;
+        this.initNumber = this.generateNumber();
+        this.init();
+    }
+
+    init() {
+        this.msg.reply(`Higher or Lower ${this.initNumber}`);
+    }
+
+    playerGuess(guess) {
+        let nextNumber = generateRandomNumber();
+        if (guess.toLowerCase() === "higher" && nextNumber > this.initNumber) {
+            this.msg.reply("you suck, little DICK");
+        } else if (guess.toLowerCase() === "lower" && nextNumber < this.initNumber) {
+            this.msg.reply("you suck, little DICK");
+        } else {
+            this.msg.reply("YOU SUCK BIG TIME");
+        }
+    }
+
+    getAuthor() {
+        return this.author;
+    }
+
+    generateNumber() {
+        return Math.floor(Math.random() * 100) + 1;
+    }
 }
 
 client.login("ODMyOTAwMjYxMDY4NTM3ODY2.YHqg0A.xpQ1_UBCZoDW_yve7fbIxSfU4I4");
